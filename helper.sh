5c434472f0b12167110fe2a626f3da5d88df66a6
Xiaoxi Pang <pangxiaoxi@gmail.com>
2018-06-10 16:32:05 -0400

    Fix the script color problem

--- helper.sh
+++ helper.sh
@@ -1,10 +1,10 @@
 #/bin/bash!
 
 # Color
-INFO='\033[38;5;29m'    # Dark Green
-TIP='\033[38;5;25m'     # Ocean Blue
-ERROR='\033[38;5;124m'  # Red
-END='\033[0m'       # No Color
+INFO='\033[32m'    # Green
+TIP='\033[34m'     # Blue
+ERROR='\033[31m'  # Red
+END='\033[39m'       # No Color
 
 # Virables
 REPO_NAME=dv-web
@@ -24,61 +24,71 @@ EOM
 install_jq(){
     # Check if the jq --version error output is zero length or not, if yes, then the jq is not installed
     if [ -z "$(jq --version 2>/dev/null)" ];then
-        echo "${ERROR}$instruction${END}"
+        echo -e "${ERROR}$instruction${END}"
     fi
 }
 
 # Build the image
 build_image(){
-    echo "\n${INFO}Build ${REPO_NAME} image...${END}"
+    echo -e "\n${INFO}Build ${REPO_NAME} image...${END}"
     docker build -f dev/Dockerfile -t ${REPO_NAME} --build-arg DEV_HOME=$(pwd) .
 }
 
+# Remove images
+remove_image(){
+    stop_and_rm_container_if_running ${REPO_NAME} &&\
+    if [[ -n "$(docker images -q ${IMAGE_NAME})" ]];then
+        echo -e "\n${INFO}Remove ${IMAGE_NAME} image...${END}" &&\
+        docker rmi ${IMAGE_NAME}
+    fi
+}
+
 # Precheck image
-has_image(){
+build_image_if_not_exist(){
     if [[ -z "$(docker images -q ${IMAGE_NAME})" ]];then
         build_image
-    else
-        echo "${INFO}${IMAGE_NAME} already exists...${END}"
+    else 
+        echo -e "${INFO}${IMAGE_NAME} already exists...${END}"
     fi
 }
 
 # Create the network
 create_network(){
     if [[ $(docker network inspect ${NETWORK_NAME} 2>/dev/null | jq '. | length') == 0 ]];then
-        echo "${INFO}Create network ${NETWORK_NAME}...${END}"
+        echo -e "${INFO}Create network ${NETWORK_NAME}...${END}"
         docker network create ${NETWORK_NAME} --driver=bridge
     else
-        echo "${INFO}${NETWORK_NAME} already exists...${END}"
+        echo -e "${INFO}${NETWORK_NAME} already exists...${END}"
     fi
 }
 
 start_app(){
     if [[ -z "$(docker ps -aqf "name=${REPO_NAME}")" ]]; then
-        echo "${INFO}Start ${REPO_NAME}..." &&\
+        echo -e "${INFO}Start ${REPO_NAME}..." &&\
         docker run --rm -it --name ${REPO_NAME} -v $(pwd):$(pwd) -p 3001:3001 --network ${NETWORK_NAME} ${IMAGE_NAME}
     else
-        echo "${INFO}${REPO_NAME} is already up...${END}"
+        echo -e "${INFO}${REPO_NAME} is already up...${END}"
     fi
 }
 
 # Run the container in command mode
 run_cli(){
     if [[ -n "$(docker ps -aqf "name=${REPO_NAME}")" ]]; then
-        echo "${INFO}exec with current up container ${REPO_NAME}..." &&\
+        echo -e "${INFO}exec with current up container ${REPO_NAME}..." &&\
         docker exec -it ${REPO_NAME} ash
     else
-        echo "${INFO}create a empheral container to access cli...${END}"
+        echo -e "${INFO}create a empheral container to access cli...${END}"
         docker run --rm -it --name ${REPO_NAME} -v $(pwd):$(pwd) -p 3001:3001 --network ${NETWORK_NAME} ${IMAGE_NAME} ash
     fi
 }
 
 
 # Stop and remove single container
-stop_and_rm_container(){
+stop_and_rm_container_if_running(){
     # Filter the specific docker container by using its name and check the output is nonzero
     if [[ -n $(docker ps -aqf "name=$1") ]]; then
-        docker stop $1
+        echo -e "${INFO}Stop and remove $1...${END}" &&\
+        docker stop $1 &&\
         # Check if the container still availabe and remove it if it is not auto removable
         if [ $(docker inspect $1 2>/dev/null | jq '.[0].HostConfig.AutoRemove') == "false" ]; then
             docker rm $1
@@ -88,17 +98,18 @@ stop_and_rm_container(){
 
 # Stop and remove all of the containers
 remove_all(){
-    echo "\n${INFO}Stop and remove all services...${END}\n"
-    echo "${INFO}Stop and remove ${REPO_NAME}...${END}" &&\
-    stop_and_rm_container ${REPO_NAME}
+    echo -e "\n${INFO}Stop and remove all services...${END}\n"
+    echo -e "${INFO}Stop and remove ${REPO_NAME}...${END}" &&\
+    stop_and_rm_container_if_running ${REPO_NAME}
 }
 
 # Main
 install_jq
 
-echo "${INFO}Select one action:${END}"
+echo -e "${INFO}Select one action:${END}"
 
 actions=(
+    "Build ${REPO_NAME} image"
     "Start ${REPO_NAME}"
     "Stop all services"
     "Run npm command through Docker"
@@ -109,20 +120,24 @@ select action in "${actions[@]}"
 do
     case ${action} in
         "${actions[0]}")
-            echo "\n${INFO}Start ${REPO_NAME} ...${END}"
-            has_image && \
-            create_network && \
-            start_app
+            remove_image && \
+            build_image
             ;;
         "${actions[1]}")
-            remove_all
+            echo -e "\n${INFO}Start ${REPO_NAME} with ES and Kibana...${END}"
+            build_image_if_not_exist && \
+            create_network && \
+            start_all
             ;;
         "${actions[2]}")
-            run_cli
+            remove_all
             ;;
         "${actions[3]}")
+            run_cli
+            ;;
+        "${actions[4]}")
             break
             ;;
-        *) echo "${ERROR} invalid action${END}";;
+        *) echo -e "${ERROR} invalid action${END}";;
     esac
 done
